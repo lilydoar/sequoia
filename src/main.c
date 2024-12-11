891f5475ee27acf327860c93408eda4e0f442efe
Lily Doar <lilydoar4@gmail.com>
2024-12-10 20:53:00 -0800

    pipeline

--- src/main.c
+++ src/main.c
@@ -4,6 +4,7 @@
 #include "SDL3/SDL_init.h"
 #include "SDL3/SDL_iostream.h"
 #include "SDL3/SDL_log.h"
+#include "SDL3/SDL_stdinc.h"
 #include "SDL3/SDL_video.h"
 
 #include <SDL3/SDL.h>
@@ -15,7 +16,8 @@
 #define WINDOW_W 800
 #define WINDOW_H 600
 
-SDL_GPUShader *loadShader(SDL_GPUDevice *device, const char *file) {
+SDL_GPUShader *loadShader(SDL_GPUDevice *device, const char *file,
+                          const char *entryPoint) {
   size_t size;
   void *contents = SDL_LoadFile(file, &size);
   if (contents == NULL) {
@@ -27,18 +29,24 @@ SDL_GPUShader *loadShader(SDL_GPUDevice *device, const char *file) {
   SDL_GPUShaderCreateInfo info = {
       .code_size = size,
       .code = contents,
-      .entrypoint = "mainVertex",
+      .entrypoint = entryPoint,
       .format = SDL_GPU_SHADERFORMAT_MSL,
       .stage = SDL_GPU_SHADERSTAGE_VERTEX,
+      .num_samplers = 0,
+      .num_storage_textures = 0,
+      .num_storage_buffers = 0,
+      .num_uniform_buffers = 0,
   };
 
   SDL_GPUShader *shader = SDL_CreateGPUShader(device, &info);
   if (shader == NULL) {
     SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Shader creation failed: %s\n",
                  SDL_GetError());
+    SDL_free(contents);
     return NULL;
   }
 
+  SDL_free(contents);
   return shader;
 }
 
@@ -83,7 +91,39 @@ int main(int argc, char **argv) {
     return 1;
   }
 
-  SDL_GPUShader *shader = loadShader();
+  const char *file = "content/shaders/metal/triangle.metal";
+  SDL_GPUShader *vertexShader = loadShader(device, file, "mainVertex");
+  if (vertexShader == NULL) {
+    return 1;
+  }
+  SDL_GPUShader *fragmentShader = loadShader(device, file, "mainFragment");
+  if (fragmentShader == NULL) {
+    return 1;
+  }
+
+  SDL_GPUColorTargetDescription description = {
+      .format = SDL_GetGPUSwapchainTextureFormat(device, window),
+  };
+  SDL_GPUGraphicsPipelineCreateInfo info = {
+      .vertex_shader = vertexShader,
+      .fragment_shader = fragmentShader,
+      .primitive_type = SDL_GPU_PRIMITIVETYPE_TRIANGLELIST,
+      .target_info =
+          {
+              .color_target_descriptions = &description,
+              .num_color_targets = 1,
+          },
+  };
+  SDL_GPUGraphicsPipeline *pipeline =
+      SDL_CreateGPUGraphicsPipeline(device, &info);
+  if (pipeline == NULL) {
+    SDL_LogError(SDL_LOG_CATEGORY_ERROR,
+                 "Graphics pipeline creation failed: %s\n", SDL_GetError());
+    return 1;
+  }
+
+  SDL_ReleaseGPUShader(device, vertexShader);
+  SDL_ReleaseGPUShader(device, fragmentShader);
 
   bool quit = false;
   while (!quit) {
