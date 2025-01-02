b9938c007856daa5833e746ea7ecc6ad737294e3
Lily Doar <lilydoar4@gmail.com>
2025-01-01 22:28:04 -0800

    build deps list

--- build.zig
+++ build.zig
@@ -1,24 +1,22 @@
 const std = @import("std");
 
-pub fn build(b: *std.Build) void {
+pub fn build(b: *std.Build) !void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const zigimg_dep = b.dependency("zigimg", .{
+    var deps = std.ArrayList([]const u8).init(b.allocator);
+    defer deps.deinit();
+
+    try deps.append("zigimg");
+
+    const lib = b.addStaticLibrary(.{
+        .name = "sequoia",
+        .root_source_file = b.path("src/root.zig"),
         .target = target,
         .optimize = optimize,
     });
-
-    // const lib = b.addStaticLibrary(.{
-    //     .name = "sequoia",
-    //     .root_source_file = b.path("src/root.zig"),
-    //     .target = target,
-    //     .optimize = optimize,
-    // });
-    //
-    // lib.linkSystemLibrary("SDL3");
-    //
-    // b.installArtifact(lib);
+    add_libraries(b, target, optimize, lib, deps);
+    b.installArtifact(lib);
 
     const exe = b.addExecutable(.{
         .name = "sequoia",
@@ -26,11 +24,7 @@ pub fn build(b: *std.Build) void {
         .target = target,
         .optimize = optimize,
     });
-
-    exe.linkSystemLibrary("SDL3");
-
-    exe.root_module.addImport("zigimg", zigimg_dep.module("zigimg"));
-
+    add_libraries(b, target, optimize, exe, deps);
     b.installArtifact(exe);
 
     const run_cmd = b.addRunArtifact(exe);
@@ -45,13 +39,13 @@ pub fn build(b: *std.Build) void {
     run_step.dependOn(&run_cmd.step);
 
     // Run unit tests
-    // const lib_unit_tests = b.addTest(.{
-    //     .root_source_file = b.path("src/root.zig"),
-    //     .target = target,
-    //     .optimize = optimize,
-    // });
-    //
-    // const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
+    const lib_unit_tests = b.addTest(.{
+        .root_source_file = b.path("src/root.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
 
     const exe_unit_tests = b.addTest(.{
         .root_source_file = b.path("src/main.zig"),
@@ -62,7 +56,7 @@ pub fn build(b: *std.Build) void {
     const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
 
     const test_step = b.step("test", "Run unit tests");
-    // test_step.dependOn(&run_lib_unit_tests.step);
+    test_step.dependOn(&run_lib_unit_tests.step);
     test_step.dependOn(&run_exe_unit_tests.step);
 
     // Compile without running
@@ -76,4 +70,22 @@ pub fn build(b: *std.Build) void {
 
     const check_step = b.step("check", "Check compilation");
     check_step.dependOn(&check_exe.step);
+}
+
+fn add_libraries(
+    b: *std.Build,
+    target: std.Build.ResolvedTarget,
+    optimize: std.builtin.OptimizeMode,
+    step: *std.Build.Step.Compile,
+    deps: std.ArrayList([]const u8),
+) void {
+    step.linkSystemLibrary("SDL3");
+
+    for (deps.items) |name| {
+        const mod = b.dependency(name, .{
+            .target = target,
+            .optimize = optimize,
+        });
+        step.root_module.addImport(name, mod.module(name));
+    }
 }
