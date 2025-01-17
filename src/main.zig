7778dde018e34dc4b526c0362a735c6bcbcf702d
Lily Doar <lilydoar4@gmail.com>
2025-01-16 23:40:21 -0800

    tmp

--- src/main.zig
+++ src/main.zig
@@ -1,24 +1,50 @@
 const std = @import("std");
+const DynLib = std.DynLib;
+
+const State = *anyopaque;
+const Result = enum { succ, fail, cont };
+
+const Context = struct {
+    dll_path: []const u8,
+    dll: std.DynLib,
+
+    state: State,
+    init_fn: *const fn () Result,
+    tick_fn: *const fn (State) Result,
+    draw_fn: *const fn (State) Result,
+    reload_fn: *const fn (State) Result,
+
+    const Self = @This();
+
+    fn load_dll(self: Self) !void {
+        if (self.dll != null) return;
+
+        var dll = try DynLib.open(self.dll_path);
+
+        self.dll = dll;
+        self.init_fn = dll.lookup(@TypeOf(self.init_fn), "init") orelse
+            return error.LookupFailure;
+        self.reload_fn = dll.lookup(@TypeOf(self.reload_fn), "reload") orelse
+            return error.LookupFailure;
+        self.tick_fn = dll.lookup(@TypeOf(self.tick_fn), "tick") orelse
+            return error.LookupFailure;
+        self.draw_fn = dll.lookup(@TypeOf(self.draw_fn), "draw") orelse
+            return error.LookupFailure;
+    }
+
+    fn unload_dll(self: Self) void {
+        if (self.dll == null) return;
+
+        self.dll.?.close();
+    }
+};
 
 pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
-
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
-
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
-
-    try bw.flush(); // don't forget to flush!
-}
-
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
+    const dll_path = "zig-out/lib/liblib.dylib";
+    const context = try Context.init(dll_path);
+    _ = context; // autofix
+    _ = context; // autofix
+    _ = context; // autofix
+    _ = context; // autofix
+    _ = context; // autofix
 }
